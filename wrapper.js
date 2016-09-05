ec81b38c3ea592854f88fd4d21d07bbd83980c0e
Alex Beregszaszi <alex@rtfs.hu>
2016-09-06 00:36:00 +0100

    Move Runtime.addFunction from wrapCallback

--- wrapper.js
+++ wrapper.js
@@ -16,7 +16,7 @@ function setupMethods (soljson) {
       soljson.setValue(ptr, buffer, '*');
     };
     var wrapCallback = function (callback) {
-      return soljson.Runtime.addFunction(function (path, contents, error) {
+      return function (path, contents, error) {
         var result = callback(soljson.Pointer_stringify(path));
         if (typeof result.contents === 'string') {
           copyString(result.contents, contents);
@@ -24,11 +24,11 @@ function setupMethods (soljson) {
         if (typeof result.error === 'string') {
           copyString(result.error, error);
         }
-      });
+      };
     };
     var compileInternal = soljson.cwrap('compileJSONCallback', 'string', ['string', 'number', 'number']);
     compileJSONCallback = function (input, optimize, readCallback) {
-      var cb = wrapCallback(readCallback);
+      var cb = soljson.Runtime.addFunction(wrapCallback(readCallback));
       var output;
       try {
         output = compileInternal(input, optimize, cb);
