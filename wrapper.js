62af72e5b4903c15a2d8e9bf3cb7a8314fe78ed2
chevdor <chevdor@gmail.com>
2016-09-10 01:26:45 +0200

    Move code out of the wrapper

--- wrapper.js
+++ wrapper.js
@@ -51,22 +51,6 @@ function setupMethods (soljson) {
       result = compileJSON(input, optimise);
     }
 
-    var res = JSON.parse(result);
-    if (res.errors) {
-      // TODO: Remove once the following is clarified:
-      // https://github.com/ethereum/solc-js/issues/53
-      // only leave the console.error
-
-      if (res.errors[0].indexOf('Warning') > 0) {
-        console.log('Compiled with Warnings: ', res.errors);
-      } else {
-        console.error('Compiled with Errors: ', res.errors);
-      }
-    } else if (res.warnings) {
-      console.warn('Compiled with Warnings: ', res.warnings);
-    } else {
-      console.log('Compiled with sucess.');
-    }
     return JSON.parse(result);
   };
 
