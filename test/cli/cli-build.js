74b90332d2619830eaf656f909c899c101035a9f
chevdor <chevdor@gmail.com>
2016-09-23 17:50:15 +0200

    Fix cli-build tests

--- test/cli/cli-build.js
+++ test/cli/cli-build.js
@@ -2,14 +2,20 @@ const tape = require('tape');
 const spawn = require('tape-spawn');
 
 tape('CLI BUILD', function (t) {
-  t.test('no mode specified', function (st) {
+  t.test('no cmd', function (st) {
     var spt = spawn(st, './solcjs test/DAO/Token.sol');
+    spt.stderr.match(/^cmd unknown/);
+    spt.end();
+  });
+
+  t.test('no mode', function (st) {
+    var spt = spawn(st, './solcjs build test/DAO/Token.sol');
     spt.stderr.match(/^Invalid option selected/);
     spt.end();
   });
 
   t.test('--bin -o output', function (st) {
-    var spt = spawn(st, './solcjs --bin test/DAO/Token.sol');
+    var spt = spawn(st, './solcjs build --bin test/DAO/Token.sol');
     spt.stderr.empty();
     // spt.stdout.empty();
     spt.succeeds();
@@ -17,13 +23,13 @@ tape('CLI BUILD', function (t) {
   });
 
   t.test('invalid file specified', function (st) {
-    var spt = spawn(st, './solcjs --bin test/fileNotFound.sol');
+    var spt = spawn(st, './solcjs build --bin test/fileNotFound.sol');
     spt.stderr.match(/^Error reading /);
     spt.end();
   });
 
   t.test('--abi -o output', function (st) {
-    var spt = spawn(st, './solcjs --abi test/DAO/Token.sol');
+    var spt = spawn(st, './solcjs build --abi test/DAO/Token.sol');
     spt.stderr.empty();
     // spt.stdout.empty();
     spt.succeeds();
@@ -31,7 +37,7 @@ tape('CLI BUILD', function (t) {
   });
 
   t.test('--bin --abi', function (st) {
-    var spt = spawn(st, './solcjs --bin --abi test/DAO/Token.sol');
+    var spt = spawn(st, './solcjs build --bin --abi test/DAO/Token.sol');
     spt.stderr.empty();
     // spt.stdout.empty();
     spt.succeeds();
