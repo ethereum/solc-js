65542e4640baf51a26aa68d119faf8cc97e052c1
chevdor <chevdor@gmail.com>
2016-11-17 21:46:54 +0100

    Linter, my love...

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 var fs = require('fs');
 
 try {
-    stats = fs.lstatSync('./soljson.js');
-	var wrapper = require('./wrapper.js');
-	module.exports = wrapper(require('./soljson.js'));
+  fs.lstatSync('./soljson.js');
+  var wrapper = require('./wrapper.js');
+  module.exports = wrapper(require('./soljson.js'));
+} catch (e) {
+  console.error('soljson.js not available.');
 }
-catch (e) {
-    console.error('soljson.js not available.');
-}
-
-
-
