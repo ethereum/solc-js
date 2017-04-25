01cb936c1052056c76978de1784662c9cbed3e18
Alex Beregszaszi <alex@rtfs.hu>
2017-04-25 15:15:39 +0100

    Translate contract output

--- translate.js
+++ translate.js
@@ -13,7 +13,50 @@ function translateJsonCompiler (output) {
     });
   }
 
-  // FIXME: translate actual output too
+  ret['contracts'] = {};
+  for (var contract in output['contracts']) {
+    // Split name first, can be `contract`, `:contract` or `filename:contract`
+    var tmp = contract.match(/^([^:]*):?([^:]+)$/);
+    var fileName, contractName;
+    if (tmp.length === 3) {
+      fileName = tmp[1];
+      contractName = tmp[2];
+    } else if (tmp.length === 2) {
+      fileName = '';
+      contractName = tmp[1];
+    } else {
+      // Force abort
+      return null;
+    }
+
+    var contractInput = output['contracts'][contract];
+    var contractOutput = {
+      'abi': contractInput['interface'],
+      'metadata': contractInput['metadata'],
+      'evm': {
+        'legacyAssembly': contractInput['assembly'],
+        'bytecode': {
+          'object': contractInput['bytecode'],
+          'opcodes': contractInput['opcodes'],
+          'sourceMap': contractInput['srcmap']
+        },
+        'deployedBytecode': {
+          'object': contractInput['runtimeBytecode'],
+          'sourceMap': contractInput['srcmapRuntime']
+        },
+        // FIXME
+        'methodIdentifiers': contractInput['methodIdentifiers'],
+        // FIXME translate
+        'gasEstimates': contractInput['gasEstimates']
+      }
+    };
+
+    if (!ret['contracts'][fileName]) {
+      ret['contracts'][fileName] = {};
+    }
+
+    ret['contracts'][fileName][contractName] = contractOutput;
+  }
 
   return ret;
 }
