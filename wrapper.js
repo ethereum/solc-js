5ede9532c75f1c831937e71ec2d95fc5d784dbc6
Alex Beregszaszi <alex@rtfs.hu>
2017-10-01 20:37:49 +0100

    Change indentation after last change

--- wrapper.js
+++ wrapper.js
@@ -6,164 +6,164 @@ var MemoryStream = require('memorystream');
 
 function setupMethods (soljson) {
   if (soljson) {
-  var compileJSON = soljson.cwrap('compileJSON', 'string', ['string', 'number']);
-  var compileJSONMulti = null;
-  if ('_compileJSONMulti' in soljson) {
-    compileJSONMulti = soljson.cwrap('compileJSONMulti', 'string', ['string', 'number']);
-  }
-  var compileJSONCallback = null;
-  var compileStandard = null;
-  if (('_compileJSONCallback' in soljson) || ('_compileStandard' in soljson)) {
-    var copyString = function (str, ptr) {
-      var buffer = soljson._malloc(str.length + 1);
-      soljson.writeStringToMemory(str, buffer);
-      soljson.setValue(ptr, buffer, '*');
-    };
-    var wrapCallback = function (callback) {
-      return function (path, contents, error) {
-        var result = callback(soljson.Pointer_stringify(path));
-        if (typeof result.contents === 'string') {
-          copyString(result.contents, contents);
-        }
-        if (typeof result.error === 'string') {
-          copyString(result.error, error);
-        }
+    var compileJSON = soljson.cwrap('compileJSON', 'string', ['string', 'number']);
+    var compileJSONMulti = null;
+    if ('_compileJSONMulti' in soljson) {
+      compileJSONMulti = soljson.cwrap('compileJSONMulti', 'string', ['string', 'number']);
+    }
+    var compileJSONCallback = null;
+    var compileStandard = null;
+    if (('_compileJSONCallback' in soljson) || ('_compileStandard' in soljson)) {
+      var copyString = function (str, ptr) {
+        var buffer = soljson._malloc(str.length + 1);
+        soljson.writeStringToMemory(str, buffer);
+        soljson.setValue(ptr, buffer, '*');
       };
-    };
+      var wrapCallback = function (callback) {
+        return function (path, contents, error) {
+          var result = callback(soljson.Pointer_stringify(path));
+          if (typeof result.contents === 'string') {
+            copyString(result.contents, contents);
+          }
+          if (typeof result.error === 'string') {
+            copyString(result.error, error);
+          }
+        };
+      };
 
-    // This calls compile() with args || cb
-    var runWithReadCallback = function (readCallback, compile, args) {
-      if (readCallback === undefined) {
-        readCallback = function (path) {
-          return {
-            error: 'File import callback not supported'
+      // This calls compile() with args || cb
+      var runWithReadCallback = function (readCallback, compile, args) {
+        if (readCallback === undefined) {
+          readCallback = function (path) {
+            return {
+              error: 'File import callback not supported'
+            };
           };
+        }
+        var cb = soljson.Runtime.addFunction(wrapCallback(readCallback));
+        var output;
+        try {
+          args.push(cb);
+          output = compile.apply(undefined, args);
+        } catch (e) {
+          soljson.Runtime.removeFunction(cb);
+          throw e;
+        }
+        soljson.Runtime.removeFunction(cb);
+        return output;
+      };
+
+      var compileInternal = soljson.cwrap('compileJSONCallback', 'string', ['string', 'number', 'number']);
+      compileJSONCallback = function (input, optimize, readCallback) {
+        return runWithReadCallback(readCallback, compileInternal, [ input, optimize ]);
+      };
+      if ('_compileStandard' in soljson) {
+        var compileStandardInternal = soljson.cwrap('compileStandard', 'string', ['string', 'number']);
+        compileStandard = function (input, readCallback) {
+          return runWithReadCallback(readCallback, compileStandardInternal, [ input ]);
         };
       }
-      var cb = soljson.Runtime.addFunction(wrapCallback(readCallback));
-      var output;
-      try {
-        args.push(cb);
-        output = compile.apply(undefined, args);
-      } catch (e) {
-        soljson.Runtime.removeFunction(cb);
-        throw e;
+    }
+
+    var compile = function (input, optimise, readCallback) {
+      var result = '';
+      if (readCallback !== undefined && compileJSONCallback !== null) {
+        result = compileJSONCallback(JSON.stringify(input), optimise, readCallback);
+      } else if (typeof input !== 'string' && compileJSONMulti !== null) {
+        result = compileJSONMulti(JSON.stringify(input), optimise);
+      } else {
+        result = compileJSON(input, optimise);
       }
-      soljson.Runtime.removeFunction(cb);
-      return output;
+      return JSON.parse(result);
     };
 
-    var compileInternal = soljson.cwrap('compileJSONCallback', 'string', ['string', 'number', 'number']);
-    compileJSONCallback = function (input, optimize, readCallback) {
-      return runWithReadCallback(readCallback, compileInternal, [ input, optimize ]);
-    };
-    if ('_compileStandard' in soljson) {
-      var compileStandardInternal = soljson.cwrap('compileStandard', 'string', ['string', 'number']);
-      compileStandard = function (input, readCallback) {
-        return runWithReadCallback(readCallback, compileStandardInternal, [ input ]);
-      };
-    }
-  }
+    // Expects a Standard JSON I/O but supports old compilers
+    var compileStandardWrapper = function (input, readCallback) {
+      if (compileStandard !== null) {
+        return compileStandard(input, readCallback);
+      }
 
-  var compile = function (input, optimise, readCallback) {
-    var result = '';
-    if (readCallback !== undefined && compileJSONCallback !== null) {
-      result = compileJSONCallback(JSON.stringify(input), optimise, readCallback);
-    } else if (typeof input !== 'string' && compileJSONMulti !== null) {
-      result = compileJSONMulti(JSON.stringify(input), optimise);
-    } else {
-      result = compileJSON(input, optimise);
-    }
-    return JSON.parse(result);
-  };
+      function formatFatalError (message) {
+        return JSON.stringify({
+          errors: [
+            {
+              'type': 'SOLCError',
+              'component': 'solcjs',
+              'severity': 'error',
+              'message': message,
+              'formattedMessage': 'Error' + message
+            }
+          ]
+        });
+      }
 
-  // Expects a Standard JSON I/O but supports old compilers
-  var compileStandardWrapper = function (input, readCallback) {
-    if (compileStandard !== null) {
-      return compileStandard(input, readCallback);
-    }
+      input = JSON.parse(input);
 
-    function formatFatalError (message) {
-      return JSON.stringify({
-        errors: [
-          {
-            'type': 'SOLCError',
-            'component': 'solcjs',
-            'severity': 'error',
-            'message': message,
-            'formattedMessage': 'Error' + message
-          }
-        ]
-      });
-    }
+      if (input['language'] !== 'Solidity') {
+        return formatFatalError('Only Solidity sources are supported');
+      }
 
-    input = JSON.parse(input);
+      if (input['sources'] == null) {
+        return formatFatalError('No input specified');
+      }
 
-    if (input['language'] !== 'Solidity') {
-      return formatFatalError('Only Solidity sources are supported');
-    }
+      // Bail out early
+      if ((input['sources'].length > 1) && (compileJSONMulti === null)) {
+        return formatFatalError('Multiple sources provided, but compiler only supports single input');
+      }
 
-    if (input['sources'] == null) {
-      return formatFatalError('No input specified');
-    }
+      function isOptimizerEnabled (input) {
+        return input['settings'] && input['settings']['optimizer'] && input['settings']['optimizer']['enabled'];
+      }
 
-    // Bail out early
-    if ((input['sources'].length > 1) && (compileJSONMulti === null)) {
-      return formatFatalError('Multiple sources provided, but compiler only supports single input');
-    }
-
-    function isOptimizerEnabled (input) {
-      return input['settings'] && input['settings']['optimizer'] && input['settings']['optimizer']['enabled'];
-    }
-
-    function translateSources (input) {
-      var sources = {};
-      for (var source in input['sources']) {
-        if (input['sources'][source]['content'] !== null) {
-          sources[source] = input['sources'][source]['content'];
-        } else {
+      function translateSources (input) {
+        var sources = {};
+        for (var source in input['sources']) {
+          if (input['sources'][source]['content'] !== null) {
+            sources[source] = input['sources'][source]['content'];
+          } else {
           // force failure
-          return null;
+            return null;
+          }
         }
+        return sources;
       }
-      return sources;
-    }
 
-    function translateOutput (output) {
-      output = translate.translateJsonCompilerOutput(JSON.parse(output));
-      if (output == null) {
-        return formatFatalError('Failed to process output');
+      function translateOutput (output) {
+        output = translate.translateJsonCompilerOutput(JSON.parse(output));
+        if (output == null) {
+          return formatFatalError('Failed to process output');
+        }
+        return JSON.stringify(output);
       }
-      return JSON.stringify(output);
-    }
 
-    var sources = translateSources(input);
-    if (sources === null) {
-      return formatFatalError('Failed to process sources');
-    }
+      var sources = translateSources(input);
+      if (sources === null) {
+        return formatFatalError('Failed to process sources');
+      }
 
-    // Try to wrap around old versions
-    if (compileJSONCallback !== null) {
-      return translateOutput(compileJSONCallback(JSON.stringify({ 'sources': sources }), isOptimizerEnabled(input), readCallback));
-    }
+      // Try to wrap around old versions
+      if (compileJSONCallback !== null) {
+        return translateOutput(compileJSONCallback(JSON.stringify({ 'sources': sources }), isOptimizerEnabled(input), readCallback));
+      }
 
-    if (compileJSONMulti !== null) {
-      return translateOutput(compileJSONMulti(JSON.stringify({ 'sources': sources }), isOptimizerEnabled(input)));
-    }
+      if (compileJSONMulti !== null) {
+        return translateOutput(compileJSONMulti(JSON.stringify({ 'sources': sources }), isOptimizerEnabled(input)));
+      }
 
-    // Try our luck with an ancient compiler
-    return translateOutput(compileJSON(sources[0], isOptimizerEnabled(input)));
-  };
+      // Try our luck with an ancient compiler
+      return translateOutput(compileJSON(sources[0], isOptimizerEnabled(input)));
+    };
 
-  var version = soljson.cwrap('version', 'string', []);
+    var version = soljson.cwrap('version', 'string', []);
 
-  var license = function () {
+    var license = function () {
     // return undefined
-  };
+    };
 
-  if ('_license' in soljson) {
-    license = soljson.cwrap('license', 'string', []);
-  }
+    if ('_license' in soljson) {
+      license = soljson.cwrap('license', 'string', []);
+    }
   }
 
   return {
