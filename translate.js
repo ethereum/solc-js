59aff3c81ff9b49c420895777ea6138467793b89
Alex Beregszaszi <alex@rtfs.hu>
2017-10-13 00:52:54 +0100

    Add legacy assembly JSON formatter
    Imported from browser-solidity (src/app/contract/contractParser.js)

--- translate.js
+++ translate.js
@@ -148,7 +148,41 @@ function translateJsonCompilerOutput (output) {
   return ret;
 }
 
+function formatAssemblyText (asm, prefix, source) {
+  if (typeof asm === typeof '' || asm === null || asm === undefined) {
+    return prefix + asm + '\n';
+  }
+  var text = prefix + '.code\n';
+  asm['.code'].forEach(function (item, i) {
+    var v = item.value === undefined ? '' : item.value;
+    var src = '';
+    if (source !== undefined && item.begin !== undefined && item.end !== undefined) {
+      src = source.slice(item.begin, item.end).replace('\n', '\\n', 'g');
+    }
+    if (src.length > 30) {
+      src = src.slice(0, 30) + '...';
+    }
+    if (item.name !== 'tag') {
+      text += '  ';
+    }
+    text += prefix + item.name + ' ' + v + '\t\t\t' + src + '\n';
+  });
+  text += prefix + '.data\n';
+  var asmData = asm['.data'] || [];
+  for (var i in asmData) {
+    var item = asmData[i];
+    text += '  ' + prefix + '' + i + ':\n';
+    text += formatAssemblyText(item, prefix + '    ', source);
+  }
+  return text;
+}
+
+function prettyPrintLegacyAssemblyJSON (assembly, source) {
+  return formatAssemblyText(assembly, '', source);
+}
+
 module.exports = {
   versionToSemver: versionToSemver,
-  translateJsonCompilerOutput: translateJsonCompilerOutput
+  translateJsonCompilerOutput: translateJsonCompilerOutput,
+  prettyPrintLegacyAssemblyJSON: prettyPrintLegacyAssemblyJSON
 };
