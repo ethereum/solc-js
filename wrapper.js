a6b830f73c38987d821395ad22dbb52866c405d6
Alex Beregszaszi <alex@rtfs.hu>
2018-09-26 17:32:17 +0100

    Make compileJSON optional

--- wrapper.js
+++ wrapper.js
@@ -48,7 +48,10 @@ function setupMethods (soljson) {
     return output;
   };
 
-  var compileJSON = soljson.cwrap('compileJSON', 'string', ['string', 'number']);
+  var compileJSON = null;
+  if ('_compileJSON' in soljson) {
+    compileJSON = soljson.cwrap('compileJSON', 'string', ['string', 'number']);
+  }
   var compileJSONMulti = null;
   if ('_compileJSONMulti' in soljson) {
     compileJSONMulti = soljson.cwrap('compileJSONMulti', 'string', ['string', 'number']);
