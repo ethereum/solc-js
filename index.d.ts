8019e930b2d684c6cea2cb29207dea33ee971e67
Alex Beregszaszi <alex@rtfs.hu>
2018-12-03 15:29:32 +0000

    Add more of the public functions to the TypeScript definition

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,15 @@
 declare module "solc" {
+  export function version(): string;
+  export function semver(): string;
+  export function license(): string;
+
+  export let features: {
+    legacySingleInput: boolean,
+    multipleInputs: boolean,
+    importCallback: boolean,
+    nativeStandardJSON: boolean,
+  };
+
   export type ReadCallbackResult = { contents: string } | { error: string };
   export type ReadCallback = (path: string) => ReadCallbackResult;
   export function compile(input: string, readCallback?: ReadCallback): string;
