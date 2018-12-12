fbc7378eb99501865578a37d8127bdd4f50ed703
Alex Beregszaszi <alex@rtfs.hu>
2018-12-12 21:52:53 +0000

    Add SMTChecker smoke tests

---
+++ test/smtchecker.js
@@ -0,0 +1,18 @@
+const tape = require('tape');
+const smtchecker = require('../smtchecker.js');
+
+tape('SMTChecker', function (t) {
+  t.test('smoke test with no axuiliaryInputRequested', function (st) {
+    var input = {};
+    var output = {};
+    st.equal(smtchecker.handleSMTQueries(input, output), null);
+    st.end();
+  });
+
+  t.test('smoke test with no smtlib2queries', function (st) {
+    var input = {};
+    var output = { auxiliaryInputRequested: {} };
+    st.equal(smtchecker.handleSMTQueries(input, output), null);
+    st.end();
+  });
+});
