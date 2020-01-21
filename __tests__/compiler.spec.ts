44159bb626aa7e13cb9f140da602183428619a98
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-01-21 20:19:47 +0100

    Updating jest setup

--- __tests__/compiler.spec.ts
+++ __tests__/compiler.spec.ts
@@ -4,7 +4,6 @@ import * as semver from "semver";
 const axios = require("axios");
 const fs = require("fs");
 import { execSync } from "child_process";
-
 /*
 import {
   getBytecode,
@@ -15,16 +14,60 @@ import {
 } from "./helpers";
 */
 
-function runTests(solc: any, versionText: string) {
+/*
+async function download(version: string) {
+  let url: string = `https://ethereum.github.io/solc-bin/bin/soljson-${version}.js`;
+  let path: string = `/tmp/${version}.js`;
+  let writer: any = fs.createWriteStream(path);
+
+  const response = await axios({
+    url,
+    method: "GET",
+    responseType: "stream"
+  });
+
+  response.data.pipe(writer);
+
+  return new Promise((resolve, reject) => {
+    writer.on("finish", resolve);
+    writer.on("error", reject);
+  });
+}
+
+// New compiler interface features 0.1.6, 0.2.1, 0.4.11, 0.5.0, etc.
+// 0.4.0 added pragmas (used in tests above)
+const versions: Array<string> = [
+  "v0.1.1+commit.6ff4cd6",
+  "v0.1.6+commit.d41f8b7",
+  "v0.2.0+commit.4dc2445",
+  "v0.2.1+commit.91a6b35",
+  "v0.3.6+commit.3fc68da",
+  "v0.4.0+commit.acd334c9",
+  "v0.4.11+commit.68ef5810",
+  "v0.4.12+commit.194ff033",
+  "v0.4.26+commit.4563c3fc",
+  "latest"
+];
+*/
+
+/*
+const solcs: Array<string> = [];
+
+async function initialize() {
+  let promises: Array<any> = [];
+  versions.forEach(version => {
+    promises.push(download(version));
+  });
+
+  let results = await Promise.all(promises);
+  versions.forEach(version => {
+    const newSolc = require("../wrapper.js")(require(`/tmp/${version}.js`));
+    solcs.push(newSolc);
+  });
+}
+*/
+function runTests(solc, versionText) {
   console.log(`Running tests with ${versionText} ${solc.version()}`);
-
-  beforeAll(() => {});
-
-  beforeEach(() => {});
-
-  afterEach(() => {});
-
-  afterAll(() => {});
 
   function getBytecode(output: any, fileName: string, contractName: string) {
     try {
@@ -39,7 +82,7 @@ function runTests(solc: any, versionText: string) {
       return "";
     }
   }
-
+  
   function getBytecodeStandard(
     output: any,
     fileName: string,
@@ -57,7 +100,7 @@ function runTests(solc: any, versionText: string) {
       return "";
     }
   }
-
+  
   function getGasEstimate(output: any, fileName: string, contractName: string) {
     try {
       var outputFile;
@@ -71,7 +114,7 @@ function runTests(solc: any, versionText: string) {
       return "";
     }
   }
-
+  
   function expectError(output: any, errorType: any, message: string) {
     if (output.errors) {
       for (let error in output.errors) {
@@ -89,7 +132,7 @@ function runTests(solc: any, versionText: string) {
     }
     return false;
   }
-
+  
   function expectNoError(output: any) {
     if (output["errors"]) {
       for (let error in output["errors"]) {
@@ -327,29 +370,40 @@ function runTests(solc: any, versionText: string) {
       }).toThrowError("Invalid callback object specified.");
     });
 
-        test('file import without lazy-loading callback fails properly (using lowlevel API)', () => {
-          // <0.2.1 doesn't have this
-          if (typeof solc.lowlevel.compileCallback !== 'function') {
-            console.log('Low-level compileCallback interface not implemented by this compiler version.');
-            return;
-          }
+    test("file import without lazy-loading callback fails properly (using lowlevel API)", () => {
+      // <0.2.1 doesn't have this
+      if (typeof solc.lowlevel.compileCallback !== "function") {
+        console.log(
+          "Low-level compileCallback interface not implemented by this compiler version."
+        );
+        return;
+      }
 
-          let input = {
-            'b.sol': 'import "a.sol"; contract B is A { function g() public { f(); } }'
-          };
-          let output = JSON.parse(solc.lowlevel.compileCallback(JSON.stringify({sources: input})));
-          expect('errors' in output).toBeTruthy();
-          // Check if the ParserError exists, but allow others too
-          expect(output.errors.length).toBeGreaterThanOrEqual(1);
-          for (let error in output.errors) {
-            // Error should be something like:
-            //   cont.sol:1:1: ParserError: Source "lib.sol" not found: File import callback not supported
-            //   cont.sol:1:1: Error: Source "lib.sol" not found: File import callback not supported
-            expect((output.errors[error].indexOf('Error') !== -1 && output.errors[error].indexOf('File import callback not supported') !== -1)
-            || output.errors[error].indexOf('not found: File import callback not supported') !== -1).toBeTruthy();
-          }
-        });
-        
+      let input = {
+        "b.sol":
+          'import "a.sol"; contract B is A { function g() public { f(); } }'
+      };
+      let output = JSON.parse(
+        solc.lowlevel.compileCallback(JSON.stringify({ sources: input }))
+      );
+      expect("errors" in output).toBeTruthy();
+      // Check if the ParserError exists, but allow others too
+      expect(output.errors.length).toBeGreaterThanOrEqual(1);
+      for (let error in output.errors) {
+        // Error should be something like:
+        //   cont.sol:1:1: ParserError: Source "lib.sol" not found: File import callback not supported
+        //   cont.sol:1:1: Error: Source "lib.sol" not found: File import callback not supported
+        expect(
+          (output.errors[error].indexOf("Error") !== -1 &&
+            output.errors[error].indexOf(
+              "File import callback not supported"
+            ) !== -1) ||
+            output.errors[error].indexOf(
+              "not found: File import callback not supported"
+            ) !== -1
+        ).toBeTruthy();
+      }
+    });
 
     test("compiling standard JSON (using lowlevel API)", () => {
       if (typeof solc.lowlevel.compileStandard !== "function") {
@@ -399,42 +453,45 @@ function runTests(solc: any, versionText: string) {
       expect(bytecodeExists(output, "b.sol", "B")).toBeTruthy();
     });
 
-        test('invalid source code fails properly with standard JSON (using lowlevel API)', () => {
-          // TODO: try finding an example which doesn't crash it?
-          if (semver.eq(solc.semver(), '0.4.11')) {
-            console.log('Skipping on broken compiler version');
-            return;
-          }
+    test("invalid source code fails properly with standard JSON (using lowlevel API)", () => {
+      // TODO: try finding an example which doesn't crash it?
+      if (semver.eq(solc.semver(), "0.4.11")) {
+        console.log("Skipping on broken compiler version");
+        return;
+      }
 
-          if (typeof solc.lowlevel.compileStandard !== 'function') {
-            console.log('Low-level compileStandard interface not implemented by this compiler version.');
-            return;
-          }
+      if (typeof solc.lowlevel.compileStandard !== "function") {
+        console.log(
+          "Low-level compileStandard interface not implemented by this compiler version."
+        );
+        return;
+      }
 
-          let input = {
-            'language': 'Solidity',
-            'settings': {
-              'outputSelection': {
-                '*': {
-                  '*': [ 'evm.bytecode' ]
-                }
-              }
-            },
-            'sources': {
-              'x.sol': {
-                'content': 'contract x { this is an invalid contract }'
-              }
+      let input = {
+        language: "Solidity",
+        settings: {
+          outputSelection: {
+            "*": {
+              "*": ["evm.bytecode"]
             }
-          };
-          let output = JSON.parse(solc.lowlevel.compileStandard(JSON.stringify(input)));
-          expect('errors' in output).toBeTruthy();
-          expect(output.errors.length).toBeGreaterThanOrEqual(1);
-          // Check if the ParserError exists, but allow others too
-          for (let error in output.errors) {
-            expect(output.errors[error].type).toBe('ParserError');
           }
-        });
-      
+        },
+        sources: {
+          "x.sol": {
+            content: "contract x { this is an invalid contract }"
+          }
+        }
+      };
+      let output = JSON.parse(
+        solc.lowlevel.compileStandard(JSON.stringify(input))
+      );
+      expect("errors" in output).toBeTruthy();
+      expect(output.errors.length).toBeGreaterThanOrEqual(1);
+      // Check if the ParserError exists, but allow others too
+      for (let error in output.errors) {
+        expect(output.errors[error].type).toBe("ParserError");
+      }
+    });
 
     test("compiling standard JSON (with callback) (using lowlevel API)", () => {
       if (typeof solc.lowlevel.compileStandard !== "function") {
@@ -711,34 +768,38 @@ function runTests(solc: any, versionText: string) {
       expect(L.length).toBeGreaterThan(0);
     });
 
-    
-        test('compiling standard JSON (with warning >=0.4.0)', () => {
-          // In 0.4.0 "pragma solidity" was added. Not including it is a warning.
-          if (semver.lt(solc.semver(), '0.4.0')) {
-            console.log('Not supported by solc');
-            return;
-          }
-  
-          let input = {
-            'language': 'Solidity',
-            'settings': {
-              'outputSelection': {
-                '*': {
-                  '*': [ 'evm.bytecode' ]
-                }
-              }
-            },
-            'sources': {
-              'c.sol': {
-                'content': 'contract C { function f() public { } }'
-              }
+    test("compiling standard JSON (with warning >=0.4.0)", () => {
+      // In 0.4.0 "pragma solidity" was added. Not including it is a warning.
+      if (semver.lt(solc.semver(), "0.4.0")) {
+        console.log("Not supported by solc");
+        return;
+      }
+
+      let input = {
+        language: "Solidity",
+        settings: {
+          outputSelection: {
+            "*": {
+              "*": ["evm.bytecode"]
             }
-          };
-  
-          let output = JSON.parse(solc.compile(JSON.stringify(input)));
-          expect(expectError(output, 'Warning', 'Source file does not specify required compiler version!')).toBeTruthy();
-        });
-        
+          }
+        },
+        sources: {
+          "c.sol": {
+            content: "contract C { function f() public { } }"
+          }
+        }
+      };
+
+      let output = JSON.parse(solc.compile(JSON.stringify(input)));
+      expect(
+        expectError(
+          output,
+          "Warning",
+          "Source file does not specify required compiler version!"
+        )
+      ).toBeTruthy();
+    });
 
     test("compiling standard JSON (using libraries) (using lowlevel API)", () => {
       // 0.4.0 has a bug with libraries
@@ -793,102 +854,97 @@ function runTests(solc: any, versionText: string) {
       expect(L.length).toBeGreaterThan(0);
     });
 
-    
-        test('compiling standard JSON (invalid JSON)', () => {
-          let output = JSON.parse(solc.compile('{invalid'));
-          // TODO: change wrapper to output matching error
-          expect(expectError(output, 'JSONError', 'Line 1, Column 2\n  Missing \'}\' or object member name') || expectError(output, 'JSONError', 'Invalid JSON supplied:')).toBeTruthy();
-        });
-  
-        test('compiling standard JSON (invalid language)', () => {
-          let output = JSON.parse(solc.compile('{"language":"InvalidSolidity","sources":{"cont.sol":{"content":""}}}'));
-          expect(expectError(output, 'JSONError', 'supported as a language.') && expectError(output, 'JSONError', '"Solidity"')).toBeTruthy();
-        });
-  
-        test('compiling standard JSON (no sources)', () => {
-          let output = JSON.parse(solc.compile('{"language":"Solidity"}'));
-          expect(expectError(output, 'JSONError', 'No input sources specified.')).toBeTruthy();
-        });
-  
-        test('compiling standard JSON (multiple sources on old compiler)', () => {
-          let output = JSON.parse(solc.compile('{"language":"Solidity","sources":{"cont.sol":{"content":"import \\"lib.sol\\";"},"lib.sol":{"content":""}}}'));
-          if (solc.features.multipleInputs) {
-            expect(expectNoError(output)).toBeTruthy();
-          } else {
-            expect(expectError(output, 'JSONError', 'Multiple sources provided, but compiler only supports single input.') || expectError(output, 'Parser error', 'Parser error: Source not found.')).toBeTruthy();
-          }
-        });
-      });
-    
-    
+    test("compiling standard JSON (invalid JSON)", () => {
+      let output = JSON.parse(solc.compile("{invalid"));
+      // TODO: change wrapper to output matching error
+      expect(
+        expectError(
+          output,
+          "JSONError",
+          "Line 1, Column 2\n  Missing '}' or object member name"
+        ) || expectError(output, "JSONError", "Invalid JSON supplied:")
+      ).toBeTruthy();
+    });
+
+    test("compiling standard JSON (invalid language)", () => {
+      let output = JSON.parse(
+        solc.compile(
+          '{"language":"InvalidSolidity","sources":{"cont.sol":{"content":""}}}'
+        )
+      );
+      expect(
+        expectError(output, "JSONError", "supported as a language.") &&
+          expectError(output, "JSONError", '"Solidity"')
+      ).toBeTruthy();
+    });
+
+    test("compiling standard JSON (no sources)", () => {
+      let output = JSON.parse(solc.compile('{"language":"Solidity"}'));
+      expect(
+        expectError(output, "JSONError", "No input sources specified.")
+      ).toBeTruthy();
+    });
+
+    test("compiling standard JSON (multiple sources on old compiler)", () => {
+      let output = JSON.parse(
+        solc.compile(
+          '{"language":"Solidity","sources":{"cont.sol":{"content":"import \\"lib.sol\\";"},"lib.sol":{"content":""}}}'
+        )
+      );
+      if (solc.features.multipleInputs) {
+        expect(expectNoError(output)).toBeTruthy();
+      } else {
+        expect(
+          expectError(
+            output,
+            "JSONError",
+            "Multiple sources provided, but compiler only supports single input."
+          ) ||
+            expectError(
+              output,
+              "Parser error",
+              "Parser error: Source not found."
+            )
+        ).toBeTruthy();
+      }
+    });
+  });
 
   // Only run on the latest version.
   if (versionText === "latest") {
     describe("Loading Legacy Versions", () => {
-            test('loading remote version - development snapshot', () => {
-              solc.loadRemoteVersion('latest', function (err, solcSnapshot) {
-                if (err) {
-                  console.log('Network error - skipping remote loading test');
-                  return;
+      test("loading remote version - development snapshot", () => {
+        solc.loadRemoteVersion("latest", function(err, solcSnapshot) {
+          if (err) {
+            console.log("Network error - skipping remote loading test");
+            return;
+          }
+          let input = {
+            language: "Solidity",
+            settings: {
+              outputSelection: {
+                "*": {
+                  "*": ["evm.bytecode"]
                 }
-                let input = {
-                  'language': 'Solidity',
-                  'settings': {
-                    'outputSelection': {
-                      '*': {
-                        '*': [ 'evm.bytecode' ]
-                      }
-                    }
-                  },
-                  'sources': {
-                    'cont.sol': {
-                      'content': 'contract x { function g() public {} }'
-                    }
-                  }
-                };
-                let output = JSON.parse(solcSnapshot.compile(JSON.stringify(input)));
-                let x = getBytecodeStandard(output, 'cont.sol', 'x');
-                expect(typeof x).toBe('string');
-                expect(x.length).toBeGreaterThanOrEqual(0);
-              });
-            });
+              }
+            },
+            sources: {
+              "cont.sol": {
+                content: "contract x { function g() public {} }"
+              }
+            }
+          };
+          let output = JSON.parse(solcSnapshot.compile(JSON.stringify(input)));
+          let x = getBytecodeStandard(output, "cont.sol", "x");
+          expect(typeof x).toBe("string");
+          expect(x.length).toBeGreaterThanOrEqual(0);
+        });
+      });
     });
   }
 }
 
-/*
-async function download(version: string) {
-  let url: string = `https://ethereum.github.io/solc-bin/bin/soljson-${version}.js`;
-  let path: string = `/tmp/${version}.js`;
-  //let writer: any = fs.createWriteStream(path); 
-
-  const response = await axios({
-    url,
-    method: 'GET',
-    responseType: 'blob'
-  })
-
-  fs.writeFileSync(path, response);
-  return version;
-
-}
-
-// First load version asynchronous
-let promises: Array<any> = [];
-versions.forEach(version => {
-    promises.push(download(version));
-})
-
-Promise.all(promises).then((versions) => {
-  versions.forEach(version => {
-    const newSolc = require('../wrapper.js')(require(`/tmp/${version}.js`));
-    runTests(newSolc, version);
-  });
-  runTests(solc, 'latest');
-})
-*/
-
-runTests(solc, "latest");
+runTests(solc, 'latest');
 
 // New compiler interface features 0.1.6, 0.2.1, 0.4.11, 0.5.0, etc.
 // 0.4.0 added pragmas (used in tests above)
