1584d306931ca49e03412fb8280cf82bc3d84c96
cameel <cameel2@gmail.com>
2021-08-27 21:03:53 +0200

    solcjs: Add --include-path option

---
+++ test/resources/importCallback/base/contractA.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.0;
+
+import "libX.sol";
+import "libY.sol";
+import "libZ.sol";
+
+contract A {}
---
+++ test/resources/importCallback/includeA/libY.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.0;
+
+import "./utils.sol";
+
+library Y {}
